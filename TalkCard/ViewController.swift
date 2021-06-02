0a42988f5c959d8641f9754226b1397e6462ceb2
miupan <twelveap.miu@gmail.com>
2021-06-02 19:03:30 +0900

    Initial Commit

---
+++ TalkCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalkCard
+//
+//  Created by 美羽 on 2021/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TalkCardTests/TalkCardTests.swift
@@ -0,0 +1,33 @@
+//
+//  TalkCardTests.swift
+//  TalkCardTests
+//
+//  Created by 美羽 on 2021/06/02.
+//
+
+import XCTest
+@testable import TalkCard
+
+class TalkCardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
